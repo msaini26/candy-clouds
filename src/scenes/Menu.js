fb7732d0a5f4b99d60c707747a32fc06f774f204
Mansi S <mansisaini26@gmail.com>
2023-05-02 15:05:39 -0700

    init title

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -10,14 +10,22 @@ class Menu extends Phaser.Scene {
         this.load.audio('sfx_select', './assets/blip_select12.wav');
         this.load.audio('sfx_explosion', './assets/explosion38.wav');
         this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+
+        // title screen background
+        this.load.image('background', './assets/background/title_sky.png'); // sky background image
     }
 
 
     // create objects and instances in phaser canvas
     create () {
+        // display title image
+        this.background = this.add.tileSprite(0, 0, 640, 480, 'background').setOrigin(0, 0); // place background tile sprite
+
+
         // set menu configurations
         let menuConfig = {
-            fontFamily:'Courier', // set font
+            fontFamily:'Helvetica', // set font
+            fontStyle: 'bold', // bold font
             fontSize: '28px', // set font size
             backgroundColor: '#F3B141', // set score background color
             color: '#843605', // set text color
@@ -27,14 +35,32 @@ class Menu extends Phaser.Scene {
                 bottom: 5,
             },
             fixedWidth: 0 // set max width
-        }
+        };
+
+        // title font configurations
+        let titleConfig = {
+            fontFamily: 'candy-shop', // set font
+            fontSize: '68px',
+            align: 'center',
+        };
+
+        // subtitle font configuration
+        let subConfig = {
+            fontFamily: 'comic-story',
+            fontSize: '28px',
+            align: 'center',
+        };
         
         // show menu text
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+        // TODO: fix circle
+        // this.circle = this.add.circle(game.config.width/2, game.config.height/4 - borderUISize - borderPadding)
+        this.add.text(game.config.width/6, game.config.height/4 - borderUISize - borderPadding, 'Candy Clouds', titleConfig);
+
+        // this.add.text(game.config.width/2, game.config.height/4 - borderUISize - borderPadding, 'CANDY CLOUDS', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', subConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
         menuConfig.color = '#000';
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', subConfig).setOrigin(0.5);
 
         // define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
